621c0ad71a172f68ac20b3ec66e1663e5a5db6c8
hiromu1995 <reonn1995@yahoo.co.jp>
2015-05-13 04:35:55 +0000

    0513 practice

---
+++ sanple01.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php入門</title>
+</head>
+<body>
+<?php
+print('phpを勉強中です');
+?>
+</body>
+</html>
---
+++ sanple02.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php入門2</title>
+</head>
+<body>
+    <?php
+    print('<h2 style="font-size; 110%; font-weight: boid">画面にHTMLを表示する</h2>');
+    print('<p>phpはHTMLの中に埋め込んで記述することができます.</p>');
+    print('<P>改行するときは、<br />brタグを記述します。</p>');
+    ?>
+</body>
+</html>
